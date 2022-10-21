1a723b1af0dd4802e5249c6b8d10dc09d0a7ca71
lepeshka25 <ruslanmuratbekov1972@gmail.com>
2022-10-21 14:13:41 +0600

    add animation frame1

--- src/App/Main/index.jsx
+++ src/App/Main/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FutureoFeSports from "../../components/FUTURE OF eSPORTS/FUTUREOFeSPORTS";
 import banner_text from '../../assets/frame1_text.svg';
 import frame1_banner from '../../assets/frame1_banner.svg';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import cs from './main.module.scss';
 
 const Main = () => {
@@ -14,27 +15,36 @@ const Main = () => {
 
 			<div className={cs.container}>
 
-				<div className={cs.left_container}>
-					<FutureoFeSports/>
+				<AnimationOnScroll className={cs.animation_container} animateIn="animate__fadeInLeft">
 
-					<div className={cs.container_absolute_text}>
-						<p>FACEBOOK</p>
-						<p>INSTAGRAM</p>
-						<p>TWITCH</p>
+					<div className={cs.left_container}>
+
+						<FutureoFeSports/>
+
+						<div className={cs.container_absolute_text}>
+							<p>FACEBOOK</p>
+							<p>INSTAGRAM</p>
+							<p>TWITCH</p>
+						</div>
+
+						<img className={cs.banner_text} src={banner_text} alt=""/>
+
+						<p className={cs.text_lorem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis, nec donec in morbi pulvinar. Enim non pulvinar neque.</p>
+
+						<div className={cs.container_btn}>
+							<button>Explore More</button>
+							<button>View our team</button>
+						</div>
 					</div>
 
-					<img className={cs.banner_text} src={banner_text} alt=""/>
+				</AnimationOnScroll>
 
-					<p className={cs.text_lorem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis, nec donec in morbi pulvinar. Enim non pulvinar neque.</p>
-
-					<div className={cs.container_btn}>
-						<button>Explore More</button>
-						<button>View our team</button>
-					</div>
-				</div>
 				<div className={cs.right_container}>
-					<img src={frame1_banner} alt=""/>
+					<AnimationOnScroll className={cs.animation_container2} animateIn="animate__fadeInRight">
+						<img src={frame1_banner} alt=""/>
+					</AnimationOnScroll>
 				</div>
+
 			</div>
 
 		</div>
