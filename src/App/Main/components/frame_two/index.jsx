3f18fb25c933f84224e87de84262315a35e2a477
lepeshka25 <ruslanmuratbekov1972@gmail.com>
2022-10-21 17:25:03 +0600

    finish frame_one

---
+++ src/App/Main/components/frame_two/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cs from './frame_two.module.scss'
+
+const FrameTwo = () => {
+	return (
+		<div className={cs.frame_two}>
+			fram2
+		</div>
+	);
+};
+
+export default FrameTwo;
--- src/App/Main/index.jsx
+++ src/App/Main/index.jsx
@@ -1,52 +1,13 @@
 import React from 'react';
-import FutureoFeSports from "../../components/FUTURE OF eSPORTS/FUTUREOFeSPORTS";
-import banner_text from '../../assets/frame1_text.svg';
-import frame1_banner from '../../assets/frame1_banner.svg';
-import { AnimationOnScroll } from 'react-animation-on-scroll';
+import FrameOne from './components/frame_one'
+import FrameTwo from "./components/frame_two";
 import cs from './main.module.scss';
 
 const Main = () => {
 	return (
 		<div className={cs.main}>
-			<div className={cs.container_background}>
-				<div className={cs.background}></div>
-				<div className={cs.background_blue}></div>
-			</div>
-
-			<div className={cs.container}>
-
-				<AnimationOnScroll className={cs.animation_container} animateIn="animate__fadeInLeft">
-
-					<div className={cs.left_container}>
-
-						<FutureoFeSports/>
-
-						<div className={cs.container_absolute_text}>
-							<p>FACEBOOK</p>
-							<p>INSTAGRAM</p>
-							<p>TWITCH</p>
-						</div>
-
-						<img className={cs.banner_text} src={banner_text} alt=""/>
-
-						<p className={cs.text_lorem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis, nec donec in morbi pulvinar. Enim non pulvinar neque.</p>
-
-						<div className={cs.container_btn}>
-							<button>Explore More</button>
-							<button>View our team</button>
-						</div>
-					</div>
-
-				</AnimationOnScroll>
-
-				<div className={cs.right_container}>
-					<AnimationOnScroll className={cs.animation_container2} animateIn="animate__fadeInRight">
-						<img src={frame1_banner} alt=""/>
-					</AnimationOnScroll>
-				</div>
-
-			</div>
-
+			<FrameOne/>
+			<FrameTwo/>
 		</div>
 	);
 };
